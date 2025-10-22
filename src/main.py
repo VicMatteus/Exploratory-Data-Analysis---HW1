c95521c91f6a90bbfbce09df593d80491e281649
Vi.Mateus <vic.matteus@alu.ufc.br>
2025-10-21 23:22:02 -0300

    criação do pca

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ import os
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
+from sklearn.preprocessing import StandardScaler
+import numpy as np
 
 def end_section():
     separator = "'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'"
@@ -122,6 +124,68 @@ def get_bivariate_analysis(df):
 
     end_section()
 
+def get_pca(df):
+    # Padronização e normalização; matriz de covariança; decomposição; principais componentes; scatterplot;
+    features_df = df.drop(columns=["classification", "condition", "uuid"])
+    target = df["classification"]
+
+    feature_names = features_df.columns
+    n_features = len(feature_names)
+
+    # Padronização
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(features_df)
+
+    cov_matrix = np.cov(X_scaled, rowvar=False)
+
+    # Decomposição
+    eigen_values, eigen_vectors = np.linalg.eig(cov_matrix)
+
+    # Seleção dos Componentes Principais
+    eigen_pairs = []
+    for i in range(len(eigen_values)):
+        pair = (np.abs(eigen_values[i]), eigen_vectors[:, i])
+        eigen_pairs.append(pair)
+        
+    # Ordenar
+    eigen_pairs.sort(key=lambda pair: pair[0], reverse=True)
+
+    # Seleciona os dois principais
+    pc1_vector = eigen_pairs[0][1].reshape(n_features, 1)
+    pc2_vector = eigen_pairs[1][1].reshape(n_features, 1)
+
+    matrix_w = np.hstack((pc1_vector, pc2_vector))
+
+    X_pca = X_scaled.dot(matrix_w)
+
+    pca_df = pd.DataFrame(data=X_pca, columns=["PC1", "PC2"])
+
+    # Adiciona a coluna classification de volta para a coloração
+    pca_df = pd.concat([pca_df, target.reset_index(drop=True)], axis=1)
+
+    user_input = input("Deseja visualizar o scatterplot do PCA? Poderá demorar um pouvo.\nS = Sim\nN = Não").upper()
+    if user_input == "S":
+        print("Gerando gráfico de dispersão PCA (PC1 vs PC2)...")
+
+        plt.figure(figsize=(12, 8))
+        sns.scatterplot(
+            x="PC1", 
+            y="PC2", 
+            hue="classification", 
+            data=pca_df,
+            alpha=0.7,
+            s=50
+        )
+
+        plt.title("Análise de Componentes Principais (PCA) - 2 Componentes")
+        plt.xlabel("Componente Principal 1 (PC1)")
+        plt.ylabel("Componente Principal 2 (PC2)")
+        plt.legend(title="Classe")
+        plt.grid(True)
+        plt.show()
+
+    end_section()
+
 def main():
     # Leitura e análise genérica inicial
     train_ds_filepath = os.path.join(os.getcwd(), "dataset", "train_data", "full_dataset_classified.csv")
@@ -139,6 +203,9 @@ def main():
 
     # Análise bivariada - parte 4
     get_bivariate_analysis(df)
+
+    # pca
+    get_pca(df)
 
 if __name__ == "__main__":
     main()
